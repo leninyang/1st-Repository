7ed59862e00cb9f38ef11e01a40e24e7a2713e53
leninyang <leninyang@gmail.com>
2015-05-27 10:12:07 -0700

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -1,1 +1,16 @@
-#!/bin/sh <br>brew cask install google-chrome <br>brew cask install dropbox <br>brew cask install box-sync <br>brew cask install caffeine <br>brew cask install 1password <br>brew cask install evernote <br>brew cask install appdelete <br>brew cask install spotify <br>brew cask install scroll-reverser <br>brew casks install skype <br>brew casks install skitch <br>brew cask install kindle <br>brew cask install karabiner <br>brew cask install vlc <br>brew casks install istat-menus
+#!/bin/sh
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install box-sync
+brew cask install caffeine
+brew cask install 1password
+brew cask install evernote
+brew cask install appdelete
+brew cask install spotify
+brew cask install scroll-reverser
+brew casks install skype
+brew casks install skitch
+brew cask install kindle
+brew cask install karabiner
+brew cask install vlc
+brew casks install istat-menus
